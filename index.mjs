422d9411f361eaf8fe343e4a4ec1426b6a03b515
Brent Gardner <bgardner@squarelabs.net>
2018-07-12 12:24:47 +0530

    Able to read file version

--- index.mjs
+++ index.mjs
@@ -6,11 +6,12 @@ const readFile = promisify(fs.readFile);
 const main = async() => {
     try {
         // https://github.com/WebAssembly/design/blob/master/BinaryEncoding.md#high-level-structure
-        const magicNumber = '\0asm'; 
         const file = await readFile('test.wasm');
-        const header = file.slice(0, magicNumber.length);
-        if(!header.every((v, i) => v === magicNumber.charCodeAt(i))) throw new Error('Invalid header!');
-        console.log('hello world', file);
+        const header = new DataView(file.buffer);
+        const magicNumber = header.getUint32(0, true);
+        if(magicNumber !== 0x6d736100) throw new Error('Invalid header!');
+        const version = header.getUint32(4, true);
+        console.log('wasm version=', version);
     } catch(ex) {
         console.error('Error reading wasm!', ex);
     }
