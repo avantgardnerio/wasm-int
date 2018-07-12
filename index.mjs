30b71bcc34338a62f663f125d21b85f36729fe01
Brent Gardner <bgardner@squarelabs.net>
2018-07-12 12:12:21 +0530

    verify magic number

--- index.mjs
+++ index.mjs
@@ -4,8 +4,16 @@ import {promisify} from 'util';
 const readFile = promisify(fs.readFile);
 
 const main = async() => {
-    const file = await readFile('test.wasm');
-    console.log('hello world', file);
+    try {
+        // https://github.com/WebAssembly/design/blob/master/BinaryEncoding.md#high-level-structure
+        const magicNumber = '\0asm'; 
+        const file = await readFile('test.wasm');
+        const header = file.slice(0, magicNumber.length);
+        if(!header.every((v, i) => v === magicNumber.charCodeAt(i))) throw new Error('Invalid header!');
+        console.log('hello world', file);
+    } catch(ex) {
+        console.error('Error reading wasm!', ex);
+    }
 }
 
 main();
