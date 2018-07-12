72406e0fed2997e6143774e547d4df0748b11c52
Brent Gardner <bgardner@squarelabs.net>
2018-07-12 11:50:23 +0530

    read file to buffer

--- index.mjs
+++ index.mjs
@@ -1,1 +1,11 @@
-console.log('hello world')
+import fs from 'fs';
+import {promisify} from 'util';
+
+const readFile = promisify(fs.readFile);
+
+const main = async() => {
+    const file = await readFile('test.wasm');
+    console.log('hello world', file);
+}
+
+main();
