3f2334b6c7d0f8cedc712369fc17dead8d1c2252
Brent Gardner <bgardner@squarelabs.net>
2018-07-14 22:36:45 +0530

    Feature/emcc tests (#1)
    * travis emcc
    
    * emcc
    
    * emcc deps
    
    * relative paths
    
    * emcc
    
    * emcc
    
    * emcc
    
    * upgraydd
    
    * deps
    
    * test
    
    * trusty
    
    * xenial
    
    * cache
    
    * cache
    
    * cache
    
    * cache
    
    * cache
    
    * cache
    
    * cache
    
    * remove logging
    
    * cache is slower

--- js/integration.test.mjs
+++ js/integration.test.mjs
@@ -2,6 +2,7 @@ import jasmine from './jasmine.mjs';
 import fs from 'fs';
 import {TextDecoder} from 'util';
 import {promisify} from 'util';
+import { execSync } from 'child_process';
 
 import WasmParser from './parser/WasmParser.mjs';
 import WasmInterpreter from './interpreter/WasmInterpreter.mjs';
@@ -16,5 +17,11 @@ jasmine.env.describe('WasmParser', () => {
         const interpreter = new WasmInterpreter([module]);
         const result = interpreter.invoke('main', 1);
         expect(result).toEqual(2);
+    })
+
+    jasmine.env.it('should compile C', () => {
+        const cmd = 'emcc hello.c -O0 -s ONLY_MY_CODE=1 -s SIDE_MODULE=1 -o index.wasm';
+        const stdout = execSync(cmd);
+        console.log(stdout);
     })
 })
