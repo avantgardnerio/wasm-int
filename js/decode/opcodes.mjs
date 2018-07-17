e3fac651113eaf7357ef61395f39e8458afa786d
Brent Gardner <bgardner@squarelabs.net>
2018-07-17 13:54:32 +0530

    i32ne

--- js/decode/opcodes.mjs
+++ js/decode/opcodes.mjs
@@ -65,7 +65,7 @@ export default {
     // https://github.com/WebAssembly/design/blob/master/BinaryEncoding.md#comparison-operators-described-here
     '0x45': notImplemented,
     '0x46': r => ({ op: 'i32.eq' }),
-    '0x47': notImplemented,
+    '0x47': r => ({ op: 'i32.ne' }),
     '0x48': notImplemented,
     '0x49': notImplemented,
     '0x4a': notImplemented,
