25c0a063a54ca748856614035f113518d5544f96
Brent Gardner <bgardner@squarelabs.net>
2018-07-26 09:41:04 +0530

    Reader test

--- js/stream/Reader.test.mjs
+++ js/stream/Reader.test.mjs
@@ -13,7 +13,7 @@ jasmine.env.describe('Reader', () => {
     jasmine.env.it('should read 64 bit zero', () => {
         const ar = new Uint8Array([0x00]);
         const reader = new Reader(ar.buffer);
-        const res = reader.readVarInt();
+        const res = reader.readVarInt64();
         expect(res).toEqual({hi: 0x00, lo: 0x00});
     });
 });
