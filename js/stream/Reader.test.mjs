8d1722ec72c2656163643e82d08098eefa0ac410
Brent Gardner <bgardner@squarelabs.net>
2018-07-26 09:39:29 +0530

    Reader test

--- js/stream/Reader.test.mjs
+++ js/stream/Reader.test.mjs
@@ -3,10 +3,17 @@ import jasmine from '../jasmine.mjs';
 import Reader from './Reader.mjs';
 
 jasmine.env.describe('Reader', () => {
-    jasmine.env.it('should read zero', () => {
+    jasmine.env.it('should read 32 bit zero', () => {
         const ar = new Uint8Array([0x00]);
         const reader = new Reader(ar.buffer);
-        const res = reader.readVarInt64();
+        const res = reader.readVarInt();
+        expect(res).toEqual(0x00);
+    });
+
+    jasmine.env.it('should read 64 bit zero', () => {
+        const ar = new Uint8Array([0x00]);
+        const reader = new Reader(ar.buffer);
+        const res = reader.readVarInt();
         expect(res).toEqual({hi: 0x00, lo: 0x00});
     });
 });
